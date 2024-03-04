211a133df07ec509aca735d7d197fc070c1e935a
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-03-04 16:29:37 +0100

    Setup prototype of basic text editor
    Note:
    Bootstrap icons used are not committed

---
+++ src/pages/editor/Editor.tsx
@@ -0,0 +1,48 @@
+// import {$getRoot, $getSelection} from 'lexical';
+// import {useEffect} from 'react';
+
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+
+import ExampleTheme from './ExampleTheme';
+import ToolbarPlugin from './plugins/ToolbarPlugin';
+
+const Placeholder = () => {
+  return <div className='editor-placeholder'>Enter some rich text...</div>;
+};
+
+const editorConfig = {
+  namespace: 'React.js Demo',
+  nodes: [],
+  // Handling of errors during update
+  onError(error: Error) {
+    throw error;
+  },
+  // The editor theme
+  theme: ExampleTheme,
+};
+
+const Editor = () => {
+  return (
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className='editor-container'>
+        <ToolbarPlugin />
+        <div className='editor-inner'>
+          <RichTextPlugin
+            contentEditable={<ContentEditable className='editor-input' />}
+            placeholder={Placeholder}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+};
+
+export default Editor;
