0b1393537497de8733542cbcbe115cdcd9bef5be
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-03-05 11:51:23 +0100

    Add quick state saving and html string conversion

--- src/pages/editor/Editor.tsx
+++ src/pages/editor/Editor.tsx
@@ -1,15 +1,19 @@
 // import {$getRoot, $getSelection} from 'lexical';
-// import {useEffect} from 'react';
+import { useState } from 'react';
 
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+import { $generateHtmlFromNodes } from '@lexical/html';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 
 import ExampleTheme from './ExampleTheme';
 import ToolbarPlugin from './plugins/ToolbarPlugin';
+import { EditorState } from 'lexical';
 
 const Placeholder = () => {
   return <div className='editor-placeholder'>Enter some rich text...</div>;
@@ -26,7 +30,29 @@ const editorConfig = {
   theme: ExampleTheme,
 };
 
+// @ts-expect-error 'leave here as reference for later use'
+// eslint-disable-next-line
+const HtmlPlugin = (): JSX.Element => {
+  const [editor] = useLexicalComposerContext();
+  const onClick = (): void => {
+    editor.update(() => {
+      const htmlString = $generateHtmlFromNodes(editor);
+      console.log(htmlString);
+      console.log(typeof htmlString);
+    });
+  };
+  return <button onClick={onClick}>HTML</button>;
+};
+
 const Editor = () => {
+  // eslint-disable-next-line
+  const [_editorState, setEditorState] = useState<EditorState>();
+
+  const onChange = (newEditorState: EditorState) => {
+    // const editorStateJSON = newEditorState.toJSON();
+    setEditorState(newEditorState);
+  };
+
   return (
     <LexicalComposer initialConfig={editorConfig}>
       <div className='editor-container'>
@@ -39,6 +65,7 @@ const Editor = () => {
           />
           <HistoryPlugin />
           <AutoFocusPlugin />
+          <OnChangePlugin onChange={onChange} />
         </div>
       </div>
     </LexicalComposer>
