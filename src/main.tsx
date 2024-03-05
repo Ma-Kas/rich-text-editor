a1eb53de596e2a43ce2b7fbfd341d62ff9ac24ca
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-03-05 16:35:12 +0100

    Recreate Lexical Playground Editor

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './styles/styles.css';
+import './pages/editor/setupEnv';
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+// Handle runtime errors
+const showErrorOverlay = (err: Event) => {
+  const ErrorOverlay = customElements.get('vite-error-overlay');
+  if (!ErrorOverlay) {
+    return;
+  }
+  const overlay = new ErrorOverlay(err);
+  const body = document.body;
+  if (body !== null) {
+    body.appendChild(overlay);
+  }
+};
+
+window.addEventListener('error', showErrorOverlay);
+window.addEventListener('unhandledrejection', ({ reason }) =>
+  showErrorOverlay(reason)
+);
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
