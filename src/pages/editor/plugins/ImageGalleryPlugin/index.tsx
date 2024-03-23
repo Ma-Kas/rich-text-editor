4445301ab11140806904abb24414c07fead10608
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-03-23 15:54:44 +0100

    Remove unused drag from GalleryPlugin

--- src/pages/editor/plugins/ImageGalleryPlugin/index.tsx
+++ src/pages/editor/plugins/ImageGalleryPlugin/index.tsx
@@ -2,24 +2,14 @@ import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext
 import { mergeRegister } from '@lexical/utils';
 import {
   $createParagraphNode,
-  $createRangeSelection,
-  $getSelection,
   $insertNodes,
-  $isNodeSelection,
-  $setSelection,
   COMMAND_PRIORITY_EDITOR,
-  COMMAND_PRIORITY_HIGH,
-  COMMAND_PRIORITY_LOW,
-  DRAGOVER_COMMAND,
-  DRAGSTART_COMMAND,
-  DROP_COMMAND,
   LexicalEditor,
 } from 'lexical';
 import { useEffect, useRef, useState } from 'react';
 import { z } from 'zod';
 
 import { INSERT_GALLERY_COMMAND } from '../../utils/exportedCommands';
-import { CAN_USE_DOM } from '../../../shared/src/canUseDOM';
 
 import landscapeImage from '../../images/landscape.jpg';
 import yellowFlowerImage from '../../images/yellow-flower.jpg';
@@ -30,7 +20,6 @@ import TextInput from '../../ui/TextInput';
 import { $createGalleryBlockNode } from '../../nodes/GalleryBlockNode';
 import {
   $createGalleryContainerNode,
-  $isGalleryContainerNode,
   GalleryContainerNode,
   GalleryImage,
 } from '../../nodes/GalleryContainerNode';
@@ -38,9 +27,6 @@ import {
 export type InsertGalleryImagePayload = GalleryImage[];
 
 const stringSchema = z.string();
-
-const getDOMSelection = (targetWindow: Window | null): Selection | null =>
-  CAN_USE_DOM ? (targetWindow || window).getSelection() : null;
 
 export function InsertGalleryImagesUriDialogBody({
   onClick,
@@ -315,133 +301,6 @@ const TRANSPARENT_IMAGE =
 const img = document.createElement('img');
 img.src = TRANSPARENT_IMAGE;
 
-function onDragStart(event: DragEvent): boolean {
-  const node = getGalleryContainerNodeInSelection();
-  if (!node) {
-    return false;
-  }
-  const dataTransfer = event.dataTransfer;
-  if (!dataTransfer) {
-    return false;
-  }
-  dataTransfer.setData('text/plain', '_');
-  dataTransfer.setDragImage(img, 0, 0);
-  dataTransfer.setData(
-    'application/x-lexical-drag',
-    JSON.stringify({
-      data: {
-        imageList: node.__imageList,
-        key: node.getKey(),
-        captionText: node.__captionText,
-        width: node.__width,
-        maxWidth: node.__maxWidth,
-      },
-      type: 'gallery-container',
-    })
-  );
-
-  return true;
-}
-
-function onDragover(event: DragEvent): boolean {
-  const node = getGalleryContainerNodeInSelection();
-  if (!node) {
-    return false;
-  }
-  if (!canDropGalleryContainer(event)) {
-    event.preventDefault();
-  }
-  return true;
-}
-
-function onDrop(event: DragEvent, editor: LexicalEditor): boolean {
-  const node = getGalleryContainerNodeInSelection();
-  if (!node) {
-    return false;
-  }
-  const data = getDragGalleryData(event);
-  if (!data) {
-    return false;
-  }
-  event.preventDefault();
-  if (canDropGalleryContainer(event)) {
-    const range = getDragSelection(event);
-    node.remove();
-    const rangeSelection = $createRangeSelection();
-    if (range !== null && range !== undefined) {
-      rangeSelection.applyDOMRange(range);
-    }
-    $setSelection(rangeSelection);
-    editor.dispatchCommand(INSERT_GALLERY_COMMAND, data);
-  }
-  return true;
-}
-
-function getGalleryContainerNodeInSelection(): GalleryContainerNode | null {
-  const selection = $getSelection();
-  if (!$isNodeSelection(selection)) {
-    return null;
-  }
-  const nodes = selection.getNodes();
-  const node = nodes[0];
-  return $isGalleryContainerNode(node) ? node : null;
-}
-
-function getDragGalleryData(
-  event: DragEvent
-): null | InsertGalleryImagePayload {
-  const dragData = event.dataTransfer?.getData('application/x-lexical-drag');
-  if (!dragData) {
-    return null;
-  }
-  const { type, data } = JSON.parse(dragData);
-  if (type !== 'gallery-container') {
-    return null;
-  }
-
-  return data;
-}
-
-declare global {
-  interface DragEvent {
-    rangeOffset?: number;
-    rangeParent?: Node;
-  }
-}
-
-function canDropGalleryContainer(event: DragEvent): boolean {
-  const target = event.target;
-  return !!(
-    target &&
-    target instanceof HTMLElement &&
-    !target.closest('code, div.EditorTheme__galleryContainer') &&
-    target.parentElement &&
-    target.parentElement.closest('div.ContentEditable__root')
-  );
-}
-
-function getDragSelection(event: DragEvent): Range | null | undefined {
-  let range;
-  const target = event.target as null | Element | Document;
-  const targetWindow =
-    target == null
-      ? null
-      : target.nodeType === 9
-        ? (target as Document).defaultView
-        : (target as Element).ownerDocument.defaultView;
-  const domSelection = getDOMSelection(targetWindow);
-  if (document.caretRangeFromPoint) {
-    range = document.caretRangeFromPoint(event.clientX, event.clientY);
-  } else if (event.rangeParent && domSelection !== null) {
-    domSelection.collapse(event.rangeParent, event.rangeOffset || 0);
-    range = domSelection.getRangeAt(0);
-  } else {
-    throw Error(`Cannot get the selection when dragging`);
-  }
-
-  return range;
-}
-
 export default function ImageGalleryPlugin(): JSX.Element | null {
   const [editor] = useLexicalComposerContext();
 
@@ -472,27 +331,6 @@ export default function ImageGalleryPlugin(): JSX.Element | null {
           return true;
         },
         COMMAND_PRIORITY_EDITOR
-      ),
-      editor.registerCommand<DragEvent>(
-        DRAGSTART_COMMAND,
-        (event) => {
-          return onDragStart(event);
-        },
-        COMMAND_PRIORITY_HIGH
-      ),
-      editor.registerCommand<DragEvent>(
-        DRAGOVER_COMMAND,
-        (event) => {
-          return onDragover(event);
-        },
-        COMMAND_PRIORITY_LOW
-      ),
-      editor.registerCommand<DragEvent>(
-        DROP_COMMAND,
-        (event) => {
-          return onDrop(event, editor);
-        },
-        COMMAND_PRIORITY_HIGH
       )
     );
   }, [editor]);
